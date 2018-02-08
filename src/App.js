ad7f828745094ffdf190d7f2c4ca5a925122179e
Rafaat Mir <theoneamir007@gmail.com>
2018-02-08 00:39:26 -0800

    Add a toggle button for the different forms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ReactButton extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			isToggled: true
+		};
+		this.onBClick = this.onBClick.bind(this);
+		
+	}
+	
+	onBClick(){
+		this.setState(previousState => ({
+			isToggled: !previousState.isToggled
+		}));
+	}
+	
+	render(){
+		return (
+			<button className="Toggle-Button" onClick={this.onBClick} type="button">{this.state.isToggled ? 'Triangle Form' : 'Square Form'}</button>
+		);
+	}
+}
+
 class Triangle extends React.Component{
 	constructor(props){
 		super();
@@ -15,14 +38,14 @@ class Triangle extends React.Component{
 	render(){
 		//var rows = [[0,0,0,0,1,0,0,0,0],[0,0,0,1,0,1,0,0,0],[0,0,1,0,2,0,1,0,0],[0,1,0,3,0,3,0,1,0],[1,0,4,0,6,0,4,0,1]];
 		return (
-			<table class="Pascals">
+			<table className="Pascals">
 				<thead>
 				</thead>
 				<tbody>
 					{this.props.rows.map((row, i) =>
 						<tr key={i}>
 							{row.map((col, j) =>
-								<td class="Num-value" key={j}>{col}</td>
+								<td className="Num-value" key={j}>{col}</td>
 							)}
 						</tr>
 					)}
@@ -53,6 +76,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Pascal's Triangle</h1>
         </header>
+		  <ReactButton />
 		  <Triangle ref="squareForm" rows={pascal}/>
 		  <br/>
 		  <Triangle ref="triangleForm" rows={pascal2}/>
